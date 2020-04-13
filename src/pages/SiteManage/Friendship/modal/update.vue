9dd798f347470a4e28143b274e36cba15b2bb17e
Jac <lu7766lu7766@gmail.com>
2020-04-13 17:11:05 +0800

    announcement manage bug fix

---
+++ src/pages/SiteManage/Friendship/modal/update.vue
@@ -0,0 +1,81 @@
+<template>
+  <detail title="编辑" @submit="doSubmit()">
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">标题</label>
+      <div class="col-md-10">
+        <validate rules="required">
+          <input type="text" class="form-control" v-model="data.title" />
+        </validate>
+      </div>
+    </div>
+
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label">图示 </label>
+      <div class="col-md-10">
+        <validate rules="img|img_width:image,370|img_height:image,165"
+                  v-slot="{ validate }">
+          <j-image alert="上传图片限制尺寸为370 × 165"
+                   :validate="validate"
+                   :src="data.image_path"
+                   :value="data"
+                   @upload="file => {data.image = file}"
+                   @delete="() => {data.del_image = Const.Y; data.image = null}"></j-image>
+        </validate>
+      </div>
+    </div>
+
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">另开视窗</label>
+      <div class="col-md-10">
+        <switcher v-model="data.is_blank" />
+      </div>
+    </div>
+
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label">链接</label>
+      <div class="col-md-10">
+        <validate rules="url">
+          <input type="text" class="form-control" v-model="data.link" />
+        </validate>
+      </div>
+    </div>
+
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">状态</label>
+      <div class="col-md-10">
+        <switcher v-model="data.enable" />
+      </div>
+    </div>
+  </detail>
+</template>
+
+<script>
+  import DetailMixins from 'mixins/Detail'
+  import EditorMixins from 'mixins/Editor'
+
+  export default {
+    mixins: [DetailMixins, EditorMixins],
+    methods: {
+      async doSubmit()
+      {
+        const data = _.cloneDeep(this.data)
+        await this.$thisApi.doUpdate(data, { formData: true })
+        this.updateSuccess()
+      },
+    },
+    mounted()
+    {
+      this.$bus.on('update.show', data =>
+      {
+        this.data = {
+          ...data,
+        }
+        this.show()
+      })
+    },
+    destroyed()
+    {
+      this.$bus.off('update.show')
+    },
+  }
+</script>
