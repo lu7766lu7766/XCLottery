bfc72bf5044cc6127fa85ce2a483fcd3a6edb6d8
Cindy <ziishe1995@gmail.com>
2020-04-22 15:39:50 +0800

    (#30)

---
+++ src/pages/LotteryResults/modal/update.vue
@@ -0,0 +1,74 @@
+<template>
+  <detail title="编辑" @submit="doSubmit()">
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">彩种</label>
+      <div class="col-md-10">
+        <validate rules="required">
+          <input v-model="data.name" :disabled="true" type="text" class="form-control">
+        </validate>
+      </div>
+    </div>
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">期数</label>
+      <div class="col-md-10">
+        <validate rules="required">
+          <input v-model="data.name" :disabled="true" type="text" class="form-control">
+        </validate>
+      </div>
+    </div>
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">开奖时间</label>
+      <div class="col-md-10">
+        <validate rules="required">
+          <input v-model="data.name" :disabled="true" type="text" class="form-control">
+        </validate>
+      </div>
+    </div>
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">开奖号码</label>
+      <div class="col-md-10">
+        <validate rules="required">
+          <div class="input-group">
+            <date-time-picker v-model="data.name" />
+            <div class="input-group-append">
+              <span class="input-group-text">
+                <i class="far fa-calendar-alt" />
+              </span>
+            </div>
+          </div>
+        </validate>
+      </div>
+    </div>
+
+    <div class="form-group row m-b-15">
+      <label class="col-md-2 col-form-label required">状态</label>
+      <div class="col-md-10">
+        <switcher v-model="data.enable" />
+      </div>
+    </div>
+  </detail>
+</template>
+
+<script>
+import DetailMixins from 'mixins/Detail'
+
+export default {
+  mixins: [DetailMixins],
+  mounted () {
+    this.$bus.on('update.show', (data) => {
+      this.data = _.cloneDeep(data)
+      this.show()
+    })
+  },
+  destroyed () {
+    this.$bus.off('update.show')
+  },
+  methods: {
+    async doSubmit () {
+      const data = _.cloneDeep(this.data)
+      await this.$thisApi.doUpdate(data)
+      this.updateSuccess()
+    }
+  }
+}
+</script>
